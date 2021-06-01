161f166cbcc4c44175bb0f08c52a0d3cd7311d03
Hafizul <75723090+HafizulShukran@users.noreply.github.com>
2021-06-01 15:36:31 +0800

    initialize & organize folder
    no actual file in the folder yet, expect initial page

---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Icon(
+        Icons.access_alarms,
+        color: Colors.red,
+        size: 30.0,
+      ),
+    );
+  }
+}
