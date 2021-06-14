0e88ba15b392681743039284713c5196b2359515
Hafizul <75723090+HafizulShukran@users.noreply.github.com>
2021-06-14 22:19:46 +0800

    update
    delete old repository

--- android/app/src/main/kotlin/com/example/easy_cook_application/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.easy_cook_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/mock_recipe.dart
+++
@@ -1,7 +0,0 @@
-import 'package:easy_cook_application/models/recipe_step.dart';
-
-import 'recipe.dart';
-
-final myRecipeList = <Recipe>[
-  Recipe(title: 'Recipe 1', step: [RecipeStep(description: 'Step 1')]),
-];
