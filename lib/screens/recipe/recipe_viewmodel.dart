8d5ce122ebcde391f51db3caf40c17341abeb996
Hafizul <75723090+HafizulShukran@users.noreply.github.com>
2021-06-16 00:16:36 +0800

    .

--- lib/screens/recipe/recipe_viewmodel.dart
+++
@@ -1,6 +0,0 @@
-import 'package:exercise3/screens/view.dart';
-
-import '../../app/dependencies.dart';
-import '../viewmodel.dart';
-
-class RecipeViewModel extends Viewmodel {}
--- lib/screens/sign_up/signup_viewmodel.dart
+++
@@ -1,2 +0,0 @@
-import '../../app/dependencies.dart';
-import '../viewmodel.dart';
