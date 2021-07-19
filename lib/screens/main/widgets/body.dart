e2e3092dc340267dcdef9f977981f619d6b3dfc2
hakiminazli <m.hakiminazli@gmail.com>
2021-07-19 16:01:27 +0800

    minor update UI

--- lib/screens/main/widgets/body.dart
+++ lib/screens/main/widgets/body.dart
@@ -8,43 +8,95 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: IconButton(
-                  icon: Icon(Icons.local_dining),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/home_recipe');
-                  },
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: IconButton(
-                  icon: Icon(Icons.redeem),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/ingredient');
-                  },
-                ),
-              ),
-            ],
+    return Container(
+      constraints: BoxConstraints.expand(),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            'assets/images/foodie.jpg',
           ),
-          Container(
-            padding: EdgeInsets.all(20.0),
-            child: IconButton(
-              icon: Icon(Icons.book),
-              onPressed: () {
-                Navigator.pushNamed(context, '/myRecipe');
-              },
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.local_dining),
+                        iconSize: 50,
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/home_recipe');
+                        },
+                      ),
+                      Text(
+                        'View Recipe',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.redeem),
+                        iconSize: 50,
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/ingredient');
+                        },
+                      ),
+                      Text(
+                        'Ingredient',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.book),
+                    iconSize: 50,
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/myRecipe');
+                    },
+                  ),
+                  Text(
+                    'Add Recipe',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
